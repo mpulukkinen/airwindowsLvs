67514a8f25e13d1f47987e9dce0324d99f30daad
Matti Pulkkinen <123762392+mpulukkinen@users.noreply.github.com>
2026-08-11 13:48:32 +0300

    Add LVS Delay VST3 factory

---
+++ lvs-plugins/plugins/Delay/DelayEntry.cpp
@@ -0,0 +1,41 @@
+#include "DelayProcessor.h"
+#include "DelayController.h"
+#include "DelayCids.h"
+
+#include "public.sdk/source/main/pluginfactory.h"
+
+#define stringPluginName "LVS Delay"
+#define FULL_VERSION_STR "0.1.0"
+
+using namespace Steinberg;
+using namespace Steinberg::Vst;
+using namespace Steinberg::LVS;
+
+BEGIN_FACTORY_DEF(
+    "Lyric Video Studio",
+    "https://lyricvideo.studio",
+    "mailto:support@lyricvideo.studio")
+
+DEF_CLASS2(
+    INLINE_UID_FROM_FUID(kDelayProcessorUID),
+    PClassInfo::kManyInstances,
+    kVstAudioEffectClass,
+    stringPluginName,
+    Vst::kDistributable,
+    LVS_DELAY_CATEGORY,
+    FULL_VERSION_STR,
+    kVstVersionString,
+    DelayProcessor::createInstance)
+
+DEF_CLASS2(
+    INLINE_UID_FROM_FUID(kDelayControllerUID),
+    PClassInfo::kManyInstances,
+    kVstComponentControllerClass,
+    stringPluginName " Controller",
+    0,
+    "",
+    FULL_VERSION_STR,
+    kVstVersionString,
+    DelayController::createInstance)
+
+END_FACTORY
