fb3b4e67107c276ce6391462148f7b7d53d3fa4a
Matti Pulkkinen <123762392+mpulukkinen@users.noreply.github.com>
2026-08-11 13:37:42 +0300

    Advertise stereo-only buses for LVS Equalizer

--- lvs-plugins/plugins/Equalizer/EqualizerProcessor.h
+++ lvs-plugins/plugins/Equalizer/EqualizerProcessor.h
@@ -15,6 +15,8 @@ public:
     }
 
     tresult PLUGIN_API initialize(FUnknown* context) SMTG_OVERRIDE;
+    tresult PLUGIN_API setBusArrangements(Vst::SpeakerArrangement* inputs, int32 numIns,
+                                          Vst::SpeakerArrangement* outputs, int32 numOuts) SMTG_OVERRIDE;
     tresult PLUGIN_API setupProcessing(Vst::ProcessSetup& setup) SMTG_OVERRIDE;
     tresult PLUGIN_API setActive(TBool state) SMTG_OVERRIDE;
     tresult PLUGIN_API canProcessSampleSize(int32 symbolicSampleSize) SMTG_OVERRIDE;
